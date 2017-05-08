d53be1ea8ffcf6ecb7f9762f7d5a2314cf20338d
as0bu <towen27@gmail.com>
2017-05-08 12:05:24 -0600

    updating WIP

--- yeastbot_app/urls.py
+++ yeastbot_app/urls.py
@@ -3,4 +3,6 @@ from yeastbot_app import views
 
 urlpatterns = [
   url(r'^$', views.index, name='index'),
+  url(r'^test/$', views.test, name='test'),
+  url(r'^yeastbot/$', views.yeastbot, name='yeastbot'),
 ]
--- yeastbot_app/views.py
+++ yeastbot_app/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, loader
+import os
+import csv
+import json
+import code
+import pprint
 
 # Create your views here.
 
 def index(request):
     return HttpResponse('Hello World!')
+
+
+def test(request):
+    return HttpResponse('This is a test')
+
+
+def yeastbot(request):
+    app_dir = os.path.dirname(__file__)
+    db_path = os.path.join(app_dir, './data/yeastbot_db.csv')
+    data = []
+    with open(db_path, 'r') as db_csv:
+        reader = csv.DictReader(db_csv)
+        for row in reader:
+            data.append(row)
+    #code.interact(local=locals())
+    template = loader.get_template('yeastbot.html')
+    return HttpResponse(template.render({'data': data}, request))
